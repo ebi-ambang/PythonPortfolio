786fd34dfc5309f0e6418234a416b3d5a4b807dd
ebi-ambang <100629554+ebi-ambang@users.noreply.github.com>
2022-11-15 14:33:13 -0500

    Add files via upload

---
+++ minandmaxnumbers.py
@@ -0,0 +1,11 @@
+first_list = [3,4,5,6]
+second_list = [-1,-2,-3,-4]
+third_list = [0,0,0,0]
+fourth_list = []
+   
+print("The maximum and minimum numbers are:",max(first_list), min(first_list))
+print("The maximum and minimum numbers are:",max(second_list), min(second_list))
+print("The maximum and minimum numbers are:",max(third_list), min(third_list))
+    
+if len(fourth_list) ==0:
+ print("None\n")
